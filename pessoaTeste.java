3b4c547ca7ce9cfd39979cafeb78536cf4ec8ac5
Monicky <164895217+M0nickyOP@users.noreply.github.com>
2025-10-29 08:47:05 -0300

    Create pessoaTeste.java

---
+++ pessoaTeste.java
@@ -0,0 +1,9 @@
+public class pessoaTeste
+{
+    public static void main(String[]args){
+    Pessoa p1 = new Pessoa("Miguel", 18,50.2, 1.50);
+    
+    System.out.println(p1.toString());
+    }
+    
+}
